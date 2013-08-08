3e5eb7f3a8df6986ec9ef3fa5d9a7ebd674c3f07
Peter Braden <peterbraden@peterbraden.co.uk>
2013-08-08 16:25:37 -0700

    interface

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ module.exports = {
 
   // function(emitter, opts, cb) -> creates a 'runner' bound to the emitter
   create: create,
+    // Must handle events:
+    // --> 'job.create' {enablePty : Boolean, repoConfig: {...}, extensionConfig: [...] }
+    // --> 'job.cancel' ?
 
   // We expose these for other modules / unit testing
   Runner : Runner
