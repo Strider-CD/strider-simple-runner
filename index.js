b242f919d795ecc3e3f28181133e5c69ee0abd95
Peter Braden <peterbraden@peterbraden.co.uk>
2013-08-08 16:22:17 -0700

    WIP

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 var Runner = require('./lib/worker')
 
-var listen = function(emitter, extensions, opts, cb){
-  var worker = new Worker({
-    pty: opts.enablePty,
-    emitter: emitter,
-    logger: opts.logger
-  })
-
-  worker.attach(emitter)
-  opts.logger.log('Simple Worker ready')
-
+var create = function(emitter, opts, cb){
+  var runner = new Runner(emitter, opts)
+  worker.attach()
   cb(null)
-
-
 }
 
 
 module.exports = {
   // Strider worker requires:
-  listen: listen,
+
+  // function(emitter, opts, cb) -> creates a 'runner' bound to the emitter
+  create: create,
 
   // We expose these for other modules / unit testing
   Runner : Runner
