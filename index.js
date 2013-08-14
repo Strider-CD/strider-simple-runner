af12e1beda11c8b27127c638ab4f96631e4a2393
Jared Forsyth <jared@jaredforsyth.com>
2013-08-14 02:00:31 -0600

    lots of refactoring. tore out lots of stuff that will go in worker-core

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-var Runner = require('./lib/worker')
-  , loader = require('strider-extension-loader')
+
+var Runner = require('./lib')
 
 var create = function(emitter, opts, cb){
   console.log(">>>>> RUNNER:: CREATE", arguments)
   var runner = new Runner(emitter, opts)
-  runner.attach()
 
   // TEMP - assume all the extensions are loaded. This
   // will be replaced when we pass the extension config
@@ -17,15 +16,18 @@ var create = function(emitter, opts, cb){
   })
 }
 
-
 module.exports = {
   // Strider worker requires:
 
   // function(emitter, opts, cb) -> creates a 'runner' bound to the emitter
   create: create,
     // Must handle events:
-    // --> 'job.create' {enablePty : Boolean, repoConfig: {...}, extensionConfig: [...] }
-    // --> 'job.cancel' ?
+    // --> 'job.new' { repo: {...}, job: {...} }
+    // --> 'job.cancel' jobid
+    // Events it may emit
+    // --> 'browser.update' (eventname, data)
+    // --> 'job.queued' jobid, time
+    // --> 'job.done' { id: jobid, commands: [...], times: {...}, test_status: int, deploy_status: int }
 
   // We expose these for other modules / unit testing
   Runner : Runner
