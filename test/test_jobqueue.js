3c044350dab7a8356fcbf2ffe3d3b0471efb3e93
Ash Wilson <smashwilson@gmail.com>
2016-03-22 15:11:06 -0400

    Test the concurrency 2 case.

--- test/test_jobqueue.js
+++ test/test_jobqueue.js
@@ -26,6 +26,26 @@ describe('JobQueue', function () {
     }
   }
 
+  // Execute a function after each queue drain. Each function should finish with an action that
+  // causes a drain to occur (push a new task or complete an active task).
+  function onEachDrain(steps, done) {
+    var wrappedSteps = []
+
+    var makeStep = function (arg) {
+      return function (cb) {
+        q.onNextDrain(cb)
+
+        arg()
+      }
+    }
+
+    for (var i = 0; i < steps.length; i++) {
+      wrappedSteps.push(makeStep(steps[i]))
+    }
+
+    async.series(wrappedSteps, done)
+  }
+
   // JobQueue handler function that manipulates handler objects set up in advance with expectJob.
   function handlerDispatch(job, config, cb) {
     var jid = job._id
@@ -62,35 +82,62 @@ describe('JobQueue', function () {
     it('waits for an available task slot when saturated', function (done) {
       expectJobs(1, 2)
 
-      async.series([
-        function (cb) {
-          q.onNextDrain(cb)
-
+      onEachDrain([
+        function () {
           q.push({ _id: 1 })
         },
-        function (cb) {
-          q.onNextDrain(cb)
-
+        function () {
           expect(handlers[1].wasCalled).to.be(true)
-
-          // The queue is now saturated.
           q.push({ _id: 2 })
         },
-        function (cb) {
-          q.onNextDrain(cb)
-
+        function () {
           expect(handlers[2].wasCalled).to.be(false)
-
-          // Finishing job 1's handler causes a slot to open, so job 2 can now run.
           handlers[1].finish()
-        },
-        function (cb) {
-          expect(handlers[2].wasCalled).to.be(true)
-
-          handlers[2].finish()
-          cb()
         }
-      ], done)
+      ], function () {
+        expect(handlers[2].wasCalled).to.be(true)
+
+        handlers[2].finish()
+        done()
+      });
     })
+  })
+
+  describe('with concurrency 2', function () {
+    beforeEach(function () {
+      q = new JobQueue(handlerDispatch, 2)
+    })
+
+    it('executes the first two tasks immediately, then waits for task completion', function (done) {
+      expectJobs(1, 2, 3)
+
+      onEachDrain([
+        function () {
+          q.push({ _id: 1 })
+        },
+        function () {
+          expect(handlers[1].wasCalled).to.be(true)
+          q.push({ _id: 2 })
+        },
+        function () {
+          expect(handlers[2].wasCalled).to.be(true)
+          q.push({ _id: 3 })
+        },
+        function () {
+          // The queue was saturated when job 3 was added. It should not have run yet
+          expect(handlers[3].wasCalled).to.be(false)
+          handlers[2].finish()
+        },
+        function () {
+          expect(handlers[3].wasCalled).to.be(true)
+
+          // Call the handlers for jobs 1 and 3 to be tidy.
+          handlers[1].finish()
+        }
+      ], function () {
+        handlers[3].finish()
+        done()
+      })
+    });
   })
 })
