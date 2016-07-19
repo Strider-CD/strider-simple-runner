bf62f357ce78be6796e1faaaab8cf33fff15afb4
Oliver Salzburg <oliver.salzburg@gmail.com>
2016-07-19 15:16:40 +0200

    Code cleanup

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
+var Runner = require('./lib');
 
-var Runner = require('./lib')
-
-var create = function(emitter, config, context, done){
-  var runner = new Runner(emitter, config)
+var create = function (emitter, config, context, done) {
+  var runner = new Runner(emitter, config);
   runner.loadExtensions(context.extensionPaths, function (err) {
     done(err, runner)
   })
-}
+};
 
 module.exports = {
   // Strider runner requires:
@@ -17,19 +16,19 @@ module.exports = {
 
   // function(emitter, opts, cb) -> creates a 'runner' bound to the emitter
   create: create,
-    // Must handle events:
-    // --> 'job.new' (job, ...
-    // --> 'job.cancel' jobid
-    // Events it is expected to emit
-    // <-- 'browser.update' eventname, data
-    // <-- 'job.queued' jobid, time
-    // <-- 'job.done' { id: jobid, commands: [...], times: {...}, test_status: int, deploy_status: int }
+  // Must handle events:
+  // --> 'job.new' (job, ...
+  // --> 'job.cancel' jobid
+  // Events it is expected to emit
+  // <-- 'browser.update' eventname, data
+  // <-- 'job.queued' jobid, time
+  // <-- 'job.done' { id: jobid, commands: [...], times: {...}, test_status: int, deploy_status: int }
 
-    // Backwards compat: TODO remove
-    // --> queue.new_job
-    // <-- queue.job_update
-    // <-- queue.job_complete
+  // Backwards compat: TODO remove
+  // --> queue.new_job
+  // <-- queue.job_update
+  // <-- queue.job_complete
 
   // We expose these for other modules / unit testing
-  Runner : Runner
-}
+  Runner: Runner
+};
